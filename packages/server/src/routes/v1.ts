d316259d6e746acee7ceb3378acff3560d63076e
cuaies <61624423+Cuaies@users.noreply.github.com>
2023-09-30 06:13:44 +0300

    feat: add route caching

--- packages/server/src/routes/v1.ts
+++ packages/server/src/routes/v1.ts
@@ -1,19 +1,38 @@
+import cache from "memory-cache";
 import { FastifyPluginCallback } from "fastify";
 import { runScraper } from "../lib/runScraper";
 import { isScraperOptions } from "../util/typeguards";
 
 export const v1: FastifyPluginCallback = (server, _, done) => {
-  server.post("/", async (req, res) => {
-    if (!req.body || !isScraperOptions(req.body)) {
+  server.get("/", async (req, res) => {
+    if (!req.query || !isScraperOptions(req.query)) {
       res.status(400).send({ error: "Invalid request body" });
       return;
     }
 
-    const scrapingData = await runScraper(req.body);
+    let scrapingData = null;
 
-    if (!scrapingData) {
-      res.status(500).send({ error: "Internal server error" });
-      return;
+    if (process.env.CACHING?.toLowerCase() === "true") {
+      scrapingData = cache.get(req.query.url);
+
+      if (!scrapingData) {
+        try {
+          scrapingData = await runScraper(req.query);
+          cache.put(req.query.url, scrapingData, 6000);
+        } catch (err) {
+          server.log.error(err);
+          res.status(500).send({ error: "Internal server error" });
+          return;
+        }
+      }
+    } else {
+      try {
+        scrapingData = await runScraper(req.query);
+      } catch (err) {
+        server.log.error(err);
+        res.status(500).send({ error: "Internal server error" });
+        return;
+      }
     }
 
     // TODO: add download option
