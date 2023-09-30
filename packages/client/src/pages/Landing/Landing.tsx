ccac0bea9afb85ad4d5dcf5f4c7f0f54f9f37a69
cuaies <61624423+Cuaies@users.noreply.github.com>
2023-09-30 10:35:47 +0300

    refactor: rename element ids

--- packages/client/src/pages/Landing/Landing.tsx
+++ packages/client/src/pages/Landing/Landing.tsx
@@ -2,29 +2,24 @@ import "./landing.scss";
 import NavBar from "../../components/navBar/NavBar";
 import { Formik, Field, Form, ErrorMessage } from "formik";
 import { isURL } from "../../validators/isURL";
-
-interface Values {
-  targetURL: "";
-  imgToggle: boolean;
-  linkToggle: boolean;
-  renderJS: boolean;
-}
+import { FormValues } from "../../ts/interface/FormValues.interface";
+import { useNavigate } from "react-router-dom";
 
 export default function Landing() {
+  const navigate = useNavigate();
+
   return (
     <div id="landing">
       <NavBar />
       <div className="container">
         <Formik
           initialValues={{
-            targetURL: "",
-            imgToggle: false,
-            linkToggle: false,
-            renderJS: false,
+            url: "",
+            fetchImages: false,
+            fetchLinks: false,
+            download: false,
           }}
-          onSubmit={async (values: Values) => {
-            // TODO: Send request to server
-          }}
+          onSubmit={async (values: FormValues) => {}}
         >
           {() => (
             <Form>
@@ -33,26 +28,26 @@ export default function Landing() {
                 <Field
                   className="text"
                   type="text"
-                  id="targetURL"
-                  name="targetURL"
+                  id="url"
+                  name="url"
                   placeholder="https://wsa-test.vercel.app"
                   validate={isURL}
                 />
-                <ErrorMessage name="targetURL" className="error" />
+                <ErrorMessage name="url" className="error" />
 
                 <label>
-                  <label htmlFor="imgToggle">Toggle Images</label>
-                  <Field type="checkbox" name="imgToggle" disabled={true} />
+                  <label htmlFor="fetchImages">Scrape Images</label>
+                  <Field type="checkbox" name="fetchImages" />
                 </label>
 
                 <label>
-                  <label htmlFor="linkToggle">Toggle Images</label>
-                  <Field type="checkbox" name="linkToggle" disabled={true} />
+                  <label htmlFor="fetchLinks">Scrape Links</label>
+                  <Field type="checkbox" name="fetchLinks" />
                 </label>
 
                 <label>
-                  <label htmlFor="renderJS">Render JavaScript</label>
-                  <Field type="checkbox" name="renderJS" disabled={true} />
+                  <label htmlFor="download">Download</label>
+                  <Field type="checkbox" name="download" />
                 </label>
 
                 <button type="submit">Submit</button>
---
+++ packages/client/src/ts/interface/FormValues.interface.ts
@@ -0,0 +1,9 @@
+/**
+ * Represents the values of the form.
+ */
+export interface FormValues {
+  url: "";
+  fetchImages: boolean;
+  fetchLinks: boolean;
+  download: boolean;
+}
