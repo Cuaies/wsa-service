08f4f6bc6fdcb55ffdb7ae989f0cb12b6db6d67c
cuaies <61624423+Cuaies@users.noreply.github.com>
2023-09-30 09:57:46 +0300

    feat(server): add support for static serving

--- packages/server/src/plugins/index.ts
+++ packages/server/src/plugins/index.ts
@@ -1,7 +1,9 @@
 import { fastifyHelmet } from "@fastify/helmet";
 import fastifyPlugin from "fastify-plugin";
 import fastifyFormbody from "@fastify/formbody";
+import fastifyStatic from "@fastify/static";
 import { router } from "../routes";
+import path from "path";
 
 /**
  * Fastify plugin to register all other plugins.
@@ -10,6 +12,9 @@ export const plugins = fastifyPlugin((server, _, done) => {
   server.register(fastifyHelmet);
   server.register(fastifyFormbody);
   server.register(router);
+  server.register(fastifyStatic, {
+    root: path.resolve("./../client/build"),
+  });
 
   done();
 });
